010b1f8ef0e71a93095e7427340f656c0287a5c7
Brian Finck <brian@brianfinck.com>
2015-06-17 00:22:05 -0700

    3.2-all working

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
 var adjective = require('./lib/adjective.js');
-var randomWord = require('./lib/randomWord.js');
+var verbs = require('./lib/verbs.js');
+var nouns = require('./lib/nouns.js');
+var randomWord = require('./lib/randomWord.js'); 
+var postRandomWord = require('./lib/postRandomWord.js');
 
 var newAdjactive = new adjective();
 
@@ -16,48 +19,15 @@ app.use(bodyparser.urlencoded({
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-var quotes = [
-    "I have not failed. Ive just found 10,000 ways that wont work. - Thomas Edison.",
-    "No matter where you go, there you are. ",
-    "If it is a good idea, go ahead and do it. It is much easier to apologize than to get permission. - Rear Admiral Grace Hopper, USN, Ph.D"
-];
-
-var verbs = function() {
-    this.fast = true;
-    this.sad = true;
-    this.soft = true;
-    this.tall = true;
-    this.round = true;
-    this.long = true;
-    this.coward = true;
-    this.beautifull = true;
-    this.bright = true;
-};
-
 var newVerb = new verbs();
-
-var nouns = function() {
-    this.city = true;
-    this.cow = true;
-    this.dog = true;
-    this.car = true;
-    this.bob = true;
-    this.seattle = true;
-    this.couch = true;
-    this.cup = true;
-    this.ball = true;
-    this.bed = true;
-    this.computer = true;
-};
 
 var newNoun = new nouns();
 
 var newAdjactive = new adjective();
 
-app.get("/quote", function (req, res){
-    var randomIndex =
-        Math.floor(Math.random()*quotes.length);
-    res.send(quotes[randomIndex]); });
+app.get('/', function(req, res) {
+    res.sendFile('index.html')		
+})
 
 app.get('/adjective', function(req, res) {
     res.json(randomWord(newAdjactive));
@@ -67,8 +37,26 @@ app.get("/verbs", function (req, res){
     res.json(randomWord(newVerb)); 
 });
 
-app.get("/nouns", function (req, res){
+app.get("/nouns", function(req, res){
     res.json(randomWord(newNoun));
+});
+
+app.post('/adjective', function(req, res) {
+    var word = postRandomWord(req.body.word, newAdjactive); 
+    console.log(req.body);
+    res.json(word);
+});
+
+app.post('/verbs', function(req, res) {
+    var word = postRandomWord(req.body.word, newVerb); 
+    console.log(req.body);
+    res.json(word);
+});
+
+app.post('/nouns', function(req, res) {
+    var word = postRandomWord(req.body.word, newNoun); 
+    console.log(req.body);
+    res.json(word);
 });
 
 app.get('/*', function(req, res) {
--- public/script.js
+++ public/script.js
@@ -21,4 +21,46 @@ $(function() {
             $("#theNoun").text(nouns);
         });
     });
+    
+    $("#submitAdjective").on("submit", function(e) {
+         e.preventDefault();
+            var adjective = $("[name=verbs]").val();
+            var adjPost;
+            if (adjective) {
+                adjPost = {word: adjective};
+                $.post("adjective", adjPost, function(response) {
+                        console.log('response');
+                        var adjectiveRes = response.msg;
+                        $("#adjRes").text(adjectiveRes);
+          })
+        }
+    });
+    
+    $("#submitVerbs").on("submit", function(e) {
+        e.preventDefault();
+        var verbs = $("[name=adjective]").val();
+        var verbPost;
+        if (verbs) {
+            verbPost = {word: verbs};
+            $.post("verbs", verbPost, function(response) {
+                console.log('response');
+                var verbsRes = response.msg;
+                $("#adjRes").text(verbsRes);
+            })
+        }
+    });
+    
+    $("#submitNouns").on("submit", function(e) {
+        e.preventDefault();
+        var nouns = $("[name=nouns]").val();
+        var nounPost;
+        if (nouns) {
+            nounPost = {word: nouns};
+            $.post("nouns", nounPost, function(response) {
+                console.log('response');
+                var nounsRes = response.msg;
+                $("#adjRes").text(nounsRes);
+            })
+        }
+    });
 });
